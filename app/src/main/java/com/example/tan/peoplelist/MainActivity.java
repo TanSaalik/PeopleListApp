17b2495bb0b82ed04b2e7e80075caa3c5307e742
TanSaalik <tan.saalik@gmail.com>
2018-09-19 13:01:27 +0300

    commit

--- app/src/main/java/com/example/tan/peoplelist/MainActivity.java
+++ app/src/main/java/com/example/tan/peoplelist/MainActivity.java
@@ -6,40 +6,69 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-
-import java.util.List;
+import java.util.ArrayList;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final PeopleList peopleList = new PeopleList();
     private EditText firstName, lastName;
-    private ListView listView;
-    private ArrayAdapter<People> adapter;
-    private List<People> list;
-
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-
 
         firstName = findViewById(R.id.firstNameInput);
         lastName = findViewById(R.id.lastNameInput);
-        listView = findViewById(R.id.listView);
+        ListView listView = findViewById(R.id.listView1);
+        Button btn = findViewById(R.id.btn_add);
+        arrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
+        listView.setAdapter(adapter);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String added = firstName.getText().toString() + " " + lastName.getText().toString();
+                arrayList.add(added);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           final int pos, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("Do you want to DELETE this name?");
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        arrayList.remove(pos);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
+            }
+        });
     }
 
     public void clear(View view) {
         firstName.setText("");
         lastName.setText("");
-        list.clear();
-        adapter.notifyDataSetChanged();
-    }
-
-    public void add(View view) {
-        adapter.notifyDataSetChanged();
     }
 }
